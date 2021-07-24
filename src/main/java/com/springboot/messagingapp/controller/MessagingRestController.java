50c8f1c35604644a8b6e4fcc9de8ae8a260fe9c0
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-07-25 00:18:35 +0530

    make Rest call to show Hello Harshit From Bridgelabz

--- src/main/java/com/springboot/messagingapp/controller/MessagingRestController.java
+++ src/main/java/com/springboot/messagingapp/controller/MessagingRestController.java
@@ -1,14 +1,23 @@
 package com.springboot.messagingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class MessagingRestController {
+
     @RequestMapping(value={"","/","/home"})
     public String sayHello(){
         return "Hello From BridgeLabz!!!";
     }
+
+    @RequestMapping(value={"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value="name")String name){
+        return "Hello" + name +"from Bridgelabz....";
+
+    }
+
 }
